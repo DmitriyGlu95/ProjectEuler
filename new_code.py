03c03a70055df77713a79776f50aa370274ff6bb
Gluschenko.Dmitriy <Dmitriy_glu@mail.ru>
2019-04-21 00:47:57 +0300

    clear

--- new_code.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.6
-current_names = ['admin', 'vasya', 'sveta', 'pasha', 'andrey']
-names = ['admin', 'Vasya', 'nina', 'sasha', 'kolya']
-mas = []
-for name in names:
-	mass.append(name.tittle())
-
-print( mass )
-
